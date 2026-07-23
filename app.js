b4474809aabf1576e010efdc141338c359275305
our-masjid <mohdshammon19@gmail.com>
2026-07-23 07:01:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('data.json')
+    fetch(`data.json?v=${Date.now()}`)
         .then(response => {
             if (!response.ok) throw new Error('JSON फाइल लोड नहीं हो पाई');
             return response.json();
